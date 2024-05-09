c085158f660f42959ecc6110e8e9026a5cacc01e
JasontheOmnivorous <marsjason485@gmail.com>
2024-05-09 19:24:40 +0630

    feat: REST API with N Layer (layered architecture)

---
+++ MTKDotNetCore.RestApiWithNLayer/ConnectionStrings.cs
@@ -0,0 +1,14 @@
+﻿namespace MTKDotNetCore.RestApiWithNLayer
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "MTKDotNetCore",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
---
+++ MTKDotNetCore.RestApiWithNLayer/Features/Blog/BlogController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCore.RestApiWithNLayer.Features.Blog
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly BL_Blog _blBlog;
+
+        public BlogController ()
+        {
+            _blBlog = new BL_Blog ();
+        }
+
+        [HttpGet]
+        public List<BlogModel> GetBlogs ()
+        {
+            var lst = _blBlog.GetBlogs();
+            return lst;
+        }
+
+        [HttpGet("{id}")]
+        public BlogModel GetBlog (int id)
+        {
+            var item = _blBlog.GetBlog(id);
+            return item;
+        }
+
+        [HttpPost]
+        public string CreateBlog (BlogModel requestModel)
+        {
+            var result = _blBlog.CreateBlog(requestModel);
+
+            string message = result > 0 ? "Saving successful!" : "Saving failed.";
+            return message;
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateBlog (int id, BlogModel requestModel)
+        {
+            var result = _blBlog.UpdateBlog(id, requestModel);
+
+            string message = result > 0 ? "Update successful!" : "Update failed.";
+            return message;
+        }
+
+        [HttpPatch("{id}")]
+        public string PatchBlog (int id, BlogModel requestModel)
+        {
+            
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteBlog (int id)
+        {
+            var result = _blBlog.DeleteBlog(id);
+
+            string message = result > 0 ? "Deleting successful!" : "Deleting failed.";
+            return message;
+        }
+    }
+}
