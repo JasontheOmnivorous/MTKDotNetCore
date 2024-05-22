a75678dabe265530b8508fcc4447aad392b38498
JasontheOmnivorous <marsjason485@gmail.com>
2024-05-22 13:49:52 +0630

    feat: integrating REST API from Console App using Refit

---
+++ MTKDotNetCore.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,13 @@
+﻿using MTKDotNetCore.ConsoleAppRefitExamples;
+
+try
+{
+    RefitExample refitExample = new RefitExample();
+    await refitExample.RunAsync();
+
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
